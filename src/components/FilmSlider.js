c947be3336edf0c9a8801574e275816797288cc7
adalbert.makarovych@gmail.com <adalbert.makarovych@gmail.com>
2019-04-14 15:13:52 +0300

    trying to find beauty styles for slider

--- src/components/FilmSlider.js
+++ src/components/FilmSlider.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
-import {Carousel} from 'react-bootstrap'
+import {Carousel, Container, Col, Row, Media} from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.css'
 import films from "../films";
 
 export default class FilmSlider extends Component {
@@ -8,12 +9,42 @@ export default class FilmSlider extends Component {
 
         const slides = images.map((image, i) =>
             <Carousel.Item>
-                <img className="d-block w-100" src={image.img} alt={i + " slide"}/>
+                <Media>
+                    <img
+                        width={900}
+                        height={900}
+                        className="mr-3"
+                        src={image.img}
+                        alt={i + " slide"}
+                    />
+                    <Media.Body>
+                        <h5>Media Heading</h5>
+                        <p>
+                            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque
+                            ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at,
+                            tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla.
+                            Donec lacinia congue felis in faucibus.
+                        </p>
+                    </Media.Body>
+                </Media>;
+
+                <Container>
+                    <Row>
+                        <Col>
+                            <Media>
+                                <img className="d-block w-100 col-xs-12 col-sm-9 col-md-4 col-lg-6" src={image.img} alt={i + " slide"}/>
+                            </Media>
+                        </Col>
+                        <Col>
+                            tuturu!
+                        </Col>
+                    </Row>
+                </Container>
             </Carousel.Item>
         )
         return(
             <div>
-                <Carousel>
+                <Carousel indicators={false}>
                     {slides}
                 </Carousel>
             </div>
