499d897f707c3a8b32e7dba2bbcd818329460cad
adalbert.makarovych@gmail.com <adalbert.makarovych@gmail.com>
2019-05-03 00:17:34 +0300

    trying to make code better

--- src/components/FilmSlider/FilmSlider.js
+++ src/components/FilmSlider/FilmSlider.js
@@ -1,47 +1,23 @@
 import React, {Component} from 'react'
-import {Carousel, Container, Col, Row, Media, Image} from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.css'
-import films from "../../films";
 import Poster from '../Poster/Poster'
 import './StyleFilmSlider.css'
 
 
 export default class FilmSlider extends Component {
     constructor(props) {
-        super(props)
+        super(props);
 
         this.state = {
-            films: [],
+            slides: [],
+            posters: [],
             width: window.innerWidth,
-            numberSlide: 0,
+            numberSlide: 0
         }
     }
-    componentDidMount() {
-        fetch('http://127.0.0.1:5000/get_films')
-            .then(response => response.json())
-            .then(data => {
-                this.setState({films:data})
-            })
-        window.addEventListener("resize", this.resize.bind(this));
-        this.resize();
-    }
-    resize() {
-        this.setState({width: window.innerWidth});
-    }
 
-    slidesLength
-    render() {
-
-        const films = this.state.films;
-        console.log((this.state.width - 150) / 400)
+    getSlides = (posters) => {
         const postPerPage = Math.max(parseInt((this.state.width - 150) / 400, 10), 1);
-
-        let posters = [];
-        if (films !== undefined) {
-            for (let i = 0; i < films.length; i++) {
-                posters.push(<Poster alt={i + " slide"} filmInfo = {films[i]}></Poster>);
-            }
-        }
 
         let slides = [];
 
@@ -55,37 +31,69 @@ export default class FilmSlider extends Component {
             slides.push(
                 <div className="Posters">
                     {temp}
-                </div>)
+                </div>);
 
-            i += postPerPage
+            i += postPerPage;
             if (i >= posters.length)
-                i -= posters.length
+                i -= posters.length;
             if (i === 0)
                 break
         }
-        this.slidesLength = slides.length;
+
+        return slides;
+    };
+
+    componentDidMount() {
+        fetch('http://127.0.0.1:5000/get_films')
+            .then(response => response.json())
+            .then(data => {
+                const films = data;
+
+                let posters = [];
+                if (films !== undefined) {
+                    for (let i = 0; i < films.length; i++) {
+                        posters.push(<Poster alt={i + " slide"} filmInfo = {films[i]}/>);
+                    }
+                }
+
+                this.setState({slides:this.getSlides(posters),
+                                     posters: posters})
+            });
+        window.addEventListener("resize", this.resize.bind(this));
+        this.resize();
+    }
+
+    resize() {
+        this.setState({width: window.innerWidth});
+        this.setState({slides:this.getSlides(this.state.posters)});
+    }
+
+    render() {
+
+        const postPerPage = Math.max(parseInt((this.state.width - 150) / 400, 10), 1);
+
         return(
             <div className="slider">
                 <button className="arrow" onClick={this.prevSlide} style={{width: parseInt((this.state.width - postPerPage*400) / 2)}}>left</button>
-                    <div>
-                        {slides[this.state.numberSlide]}
-                    </div>
+                <div>
+                    {this.state.slides[this.state.numberSlide]}
+                </div>
                 <button className="arrow" onClick={this.nextSlide} style={{width: parseInt((this.state.width - postPerPage*400) / 2)}}>right</button>
             </div>
         )
     }
+
     prevSlide = () => {
         if (this.state.numberSlide >= 1){
             this.setState({numberSlide: this.state.numberSlide - 1})
         } else {
-            this.setState({numberSlide: this.slidesLength - 1})
+            this.setState({numberSlide: this.state.slides.length - 1})
         }
-
-    }
+    };
 
     nextSlide = () => {
-        if (this.state.numberSlide < this.slidesLength - 1){
-            this.setState({numberSlide: this.state.numberSlide + 1})
+        if (this.state.numberSlide < this.state.slides.length - 1){
+            this.setState({numberSlide: (this.state.numberSlide + 1)})
         } else {
             this.setState({numberSlide: 0})
         }
