6b9fc6763bb493e6150174a5c2b87d6fe8e9124b
adalbert.makarovych@gmail.com <adalbert.makarovych@gmail.com>
2019-05-27 15:40:17 +0300

    aded button for editing profile

--- src/components/InfoField/InfoField.js
+++ src/components/InfoField/InfoField.js
@@ -1,15 +1,17 @@
 import React, {Component} from 'react'
 import "./StyleInfoField.css"
+import {connect} from "react-redux";
+import editTex from "./textures/edit_tex.png";
 
 
-export default class InfoField extends Component {
+class InfoField extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
             name: "",
             photoURL: "",
-            info: ""
+            canBeEdited: false
         };
 
         fetch("http://127.0.0.1:5000/get_user/" + this.props.ID,
@@ -43,15 +45,47 @@ export default class InfoField extends Component {
                     name: data.result.username,
                     photoURL: data.result.photo
                 });
-            })
+            });
+
+        if(nextProps.isAuthorized) {
+            fetch("http://127.0.0.1:5000/getCurUserID",
+                {
+                    method: "POST",
+                    headers: new Headers({
+                        'content-type': 'application/json'
+                    }),
+                    body: JSON.stringify({
+                        'token': localStorage.getItem("userToken")
+                    })
+                })
+                .then(response => response.json())
+                .then(curUserID => {
+                    this.setState({canBeEdited:curUserID.ID == this.props.ID});
+                })
+        }
     }
 
     render() {
         return (
             <div className="info-fieldd">
-                <img src={this.state.photoURL} alt=""/>
+                <img className="profile-photo" src={this.state.photoURL} alt=""/>
+                {this.state.canBeEdited ? (
+                    <div className="edit-button"/*onClick={this.props.startLogIn}*/> <img alt="tutu" src={editTex}/></div>
+                ) : (
+                    <div/>
+                )
+                }
                 <div className="name">{this.state.name}</div>
+
             </div>
         )
     }
 }
+
+function mapStateToProps(state) {
+    return {
+        isAuthorized: state.authorization.isAuthorized
+    }
+}
+
+export default connect(mapStateToProps)(InfoField)
